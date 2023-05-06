2424f5ff6ffa9a1f5ad2217719bd734c7eb5ce02
Anna Wallace <123022645+failed-gymnast@users.noreply.github.com>
2023-05-06 11:19:25 -0400

    Update Viz Code Chunk.R

--- Viz Code Chunk.R
+++ Viz Code Chunk.R
@@ -7,8 +7,7 @@ iris %>%
        y = 'Petal Width') +
   theme_classic() + # theme with no grid lines
   theme(
-    axis.title.y = element_text(face = 'bold', color = 'black', # creates bold labels and space around the margins of the labels
-                                margin = margin(t = 0, r = 15, b = 0, l = 5),size=13), 
+    axis.title.y = element_text(face = 'bold', color = 'black', margin = margin(t = 0, r = 15, b = 0, l = 5),size=13), # creates bold labels and space around the margins of the labels
     axis.title.x = element_text(face = 'bold', color = 'black',
                                 margin = margin(t = 15, r = 0, b = 5, l = 0),size=13),
     axis.text.x = element_text(size = 11),  # enlarges the font size of the x and y axis values
